54e744831b8679ad4dcc8f7dab6c740e0676a5e8
João Victor <joaovr.costa@gmail.com>
2022-02-04 14:10:00 -0300

    Adicionado login com firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,54 @@
 // import GoogleIcon from '../../assets/images/googleIcon.png';
 // import FacebookIcon from '../../assets/images/facebookIcon.png';
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../../services/firebase-config'
 import styles from "./Login.module.scss";
+import { useState } from "react";
 
 
 export function Login() {
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser)
+  })
+
+
+  const login = async () => {
+    try {
+       const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+    } catch (error) {
+      console.log(error.message);
+      console.log(error)
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  }
+
+
   return (
     <>
     <h1>Entre com a sua conta</h1>
     <label >E-mail</label>
-    <input placeholder="Digite seu e-mail" type="text" className={styles.input}/>
+    <input onChange={(event) => setLoginEmail(event.target.value)} placeholder="Digite seu e-mail" type="text" className={styles.input}/>
     <label >Senha</label>
-    <input placeholder="Digite sua senha" type="password" className={styles.input} />
-    <button className={styles.button}>Entrar</button>
+    <input  onChange={(event) => setLoginPassword(event.target.value)} placeholder="Digite sua senha" type="password" className={styles.input} />
+    <button onClick={login} className={styles.button}>Entrar</button>
+    <h4>User current logged:</h4>
+    {user?.email}
+    <button onClick={logout}>Logout</button>
     </>
   );
 }
--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '../../services/firebase-config'
-
+import styles from "./Signup.module.scss";
 
 export function SignUp() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
 
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser)
+  })
+
   const register = async () => {
     try {
-       createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+       const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
@@ -22,15 +28,21 @@ export function SignUp() {
     }
   };
 
+  const logout = async () => {
+    await signOut(auth);
+  }
 
   return (
     <div>
     <h1>Começe agora mesmo</h1>
     <label >E-mail</label>
-    <input  onChange={(event) => setRegisterEmail(event.target.value)} placeholder="exemplo@exemplo.com" type="text" />
+    <input  onChange={(event) => setRegisterEmail(event.target.value)} placeholder="exemplo@exemplo.com" type="text" className={styles.input} />
     <label >Senha</label>
-    <input onChange={(event) => setRegisterPassword(event.target.value)} placeholder="Digite uma senha forte" type="password"  />
-    <button  onClick={register}>Registre-se</button>
+    <input onChange={(event) => setRegisterPassword(event.target.value)} placeholder="Digite uma senha forte" type="password" className={styles.input}  />
+    <button className={styles.button} onClick={register}>Registre-se</button>
+    <h4>User current logged:{user?.email}</h4>
+    <button onClick={logout}>Logout</button>
+    
     </div>
   );
 }
