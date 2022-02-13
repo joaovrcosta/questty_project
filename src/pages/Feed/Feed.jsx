49f7f5806664c273823f7d7c4f8990acdc9eebda
Eduardo <eduardo.benigno15@gmail.com>
2022-02-13 18:34:44 -0300

    avanzando feed page

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,24 +1,54 @@
-import { useContext } from 'react';
-import styles from './Feed.module.scss'
-import { QuestsContext } from '../../contexts/QuestsContexts'
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import styles from "./Feed.module.scss";
+import { QuestsContext } from "../../contexts/QuestsContexts";
+import { Link } from "react-router-dom";
+import { Footer } from "../../components/Footer/Footer";
 
 export function Feed() {
   const { quests } = useContext(QuestsContext);
 
   return (
     <>
-    {quests.map(quest => {
-      return (
-        <form className={styles.tweet} key={quest.id}>
-          <h1>{quest.title}</h1>
-          <p>{quest.text}</p>
-          <span>{quest.category}</span>
-          <span>{new Intl.DateTimeFormat('pt-BR').format(new Date(quest.createdAt))}</span>
-          <Link to={"tarefa/"+quest.id}><button className={styles.response}>Responder</button></Link>
-        </form>
-      )
-    })}
+      <div className={styles.feedContainer}>
+        <div className={styles.feedContainerQuestionButton}>
+          <button>Perguntar</button>
+        </div>
+
+        <div className={styles.feedContainerFilterButton}>
+          <button>Matérias</button>
+        </div>
+
+        <div className={styles.feedContainerFilterButton}>
+          <button>Todas</button>
+        </div>
+
+        <div className={styles.feedContainerFetchMore}>
+          <button>+6 Perguntas</button>
+        </div>
+
+        {quests.map((quest) => {
+          return (
+            <div className={styles.questionContainer} key={quest.id}>
+              <div className={styles.questionContainerText}>
+                <p>{quest.title}</p>
+                <p>{quest.text}</p>
+                <span>{quest.category}</span>
+                <span>
+                  {new Intl.DateTimeFormat("pt-BR").format(
+                    new Date(quest.createdAt)
+                  )}
+                </span>
+              </div>
+              <Link to={"tarefa/" + quest.id}>
+                <button className={styles.questionContainerResponse}>
+                  Responder
+                </button>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <Footer />
     </>
   );
 }
