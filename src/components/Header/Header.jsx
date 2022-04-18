4a80635a72a0144c6866d2aeb917e0f707a6668e
Eduardo <eduardo.benigno15@gmail.com>
2022-04-18 11:52:23 -0300

    fixing logged header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import { useUserContext } from "../../contexts/UserContext";
+import questtyLogo from "../../assets/images/questty-logo.svg";
+import avatar03 from "../../assets/images/avatar-03.svg";
+import lupaIcon from "../../assets/images/lupa-icon.svg";
+
+const Header = ({ onOpenNewQuestForm }) => {
+  const { user, logoutUser } = useUserContext();
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.headerContainer}>
+        <Link to="/" className={styles.headerContainerLogo}>
+          <img
+            className={styles.headerContainerLogoImage}
+            src={questtyLogo}
+            alt="Questty Logo"
+          />
+        </Link>
+        <div className={styles.headerContainerSearch}>
+          <input
+            className={styles.headerContainerSearchInput}
+            placeholder="Qual a sua dúvida?"
+          ></input>
+          <img
+            className={styles.headerContainerSearchLogo}
+            src={lupaIcon}
+            alt=""
+          />
+        </div>
+        <Link to="profile" className={styles.headerContainerImage}>
+          <img
+            className={styles.headerContainerImageLink}
+            src={avatar03}
+            alt=""
+          />
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
