95b67be043b55b2a6d2729a83fb98c67c60c201c
Eduardo <eduardo.benigno15@gmail.com>
2022-04-20 15:46:45 -0300

    SignUp texto de termos e icones sociais

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -2,6 +2,10 @@ import React, { useRef } from "react";
 import { useUserContext } from "../../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
 import styles from "./Signup.module.scss";
+import { Link } from "react-router-dom";
+import googleIcon from "../../assets/images/google-icon.svg";
+import facebookIcon from "../../assets/images/facebook-icon.svg";
+
 
 const SignUp = () => {
   let navigate = useNavigate();
@@ -22,13 +26,26 @@ const SignUp = () => {
   return (
     <>
       <div className={styles.registerContainer}>
-        <p className={styles.registerContainerTitle}>Começe agora mesmo</p>
+        <p className={styles.registerContainerTitle}>Comece agora mesmo</p>
         <form className={styles.registerContainerForm} onSubmit={onSubmit}>
           <input className={styles.registerContainerFormInput} placeholder="Digite seu e-mail" type="email" ref={emailRef} />
           <button className={styles.registerContainerFormSubmitButton} type="submit">Registrar</button>
         </form>
+        <div className={styles.registerContainerTerms}>
+          <p className={styles.registerContainerTermsText}>
+            Ao se inscrever no Questty você concorda com os <Link to="/">Termos de Serviço</Link> e <Link to="/">Política de Privacidade do Questty</Link>.
+          </p>
+        </div>
 
-        <p></p>
+        <div className={styles.registerContainerSocial}>
+          <p className={styles.registerContainerSocialTitle}>
+            Ou inscreva-se usando:
+          </p>
+          <div className={styles.registerContainerSocialRowIcons}>
+            <img className={styles.registerContainerSocialRowIconsImage} src={googleIcon} alt="" />
+            <img className={styles.registerContainerSocialRowIconsImage} src={facebookIcon} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );
