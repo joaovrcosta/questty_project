b0deb5b5abea3e6a65ebc95c524e410c29a3df1b
João Victor <joaovr.costa@gmail.com>
2022-05-08 19:32:44 -0300

    added new components (button, input, select)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,40 @@
-import React, { useRef } from "react";
-import { useUserContext } from "../../contexts/UserContext";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import styles from "./Login.module.scss";
 
 const Signin = () => {
-  let navigate = useNavigate();
-  const emailRef = useRef();
-  const psdRef = useRef();
-  const { signInUser, forgotPassword } = useUserContext();
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errMsg, seterrMsg] = useState('')
+
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const email = emailRef.current.value;
-    const password = psdRef.current.value;
-    if (email && password) signInUser(email, password);
-    navigate("/feed");
-  };
-
-  const forgotPasswordHandler = () => {
-    const email = emailRef.current.value;
-    if (email)
-      forgotPassword(email).then(() => {
-        emailRef.current.value = "";
-      });
   };
 
   return (
     <div className={styles.loginContainer}>
       <p className={styles.loginContainerTitle}>Entre com a sua conta:</p>
       <form className={styles.loginContainerForm} onSubmit={onSubmit}>
-        <input className={styles.loginContainerFormInput} placeholder="Digite seu e-mail" type="email" ref={emailRef} />
-        <input className={styles.loginContainerFormInput} placeholder="Digite sua senha" type="password" ref={psdRef} />
-        <p className={styles.loginContainerFormForgotButton} onClick={forgotPasswordHandler}>Esqueci a minha senha</p>
-        <button className={styles.loginContainerFormSubmitButton} type="submit">Entrar</button>
+        <input
+          className={styles.loginContainerFormInput}
+          placeholder="Digite seu e-mail"
+          type="email"
+          value={username}
+          onChange={({target}) => setUsername (target.value) }
+        />
+        <input
+          className={styles.loginContainerFormInput}
+          placeholder="Digite sua senha"
+          type="password"
+          value={password}
+          onChange={({target}) => setPassword (target.value) }
+        />
+        <p className={styles.loginContainerFormForgotButton}>
+          Esqueci a minha senha
+        </p>
+        <button className={styles.loginContainerFormSubmitButton} type="submit">
+          Entrar
+        </button>
       </form>
     </div>
   );
--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,39 +1,56 @@
-import React, { useRef } from "react";
-import { useUserContext } from "../../contexts/UserContext";
-import { useNavigate } from "react-router-dom";
 import styles from "./Signup.module.scss";
 import { Link } from "react-router-dom";
 import googleIcon from "../../assets/images/google-icon.svg";
 import facebookIcon from "../../assets/images/facebook-icon.svg";
-
+import axios from "axios";
+import { useState } from "react";
 
 const SignUp = () => {
-  let navigate = useNavigate();
-  const emailRef = useRef();
-  const nameRef = useRef();
-  const psdRef = useRef();
-  const { registerUser } = useUserContext();
+  const [emailReg, setEmailReg] = useState('')
 
-  const onSubmit = (e) => {
+
+  const registerUser = () => {
+    axios
+      .post("http://localhost:3333/students", {
+        email: emailReg,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const email = emailRef.current.value;
-    const name = nameRef.current.value;
-    const password = psdRef.current.value;
-    if (email && password && name) registerUser(email, password, name);
-    navigate("/feed");
   };
 
   return (
     <>
       <div className={styles.registerContainer}>
         <p className={styles.registerContainerTitle}>Comece agora mesmo</p>
-        <form className={styles.registerContainerForm} onSubmit={onSubmit}>
-          <input className={styles.registerContainerFormInput} placeholder="Digite seu e-mail" type="email" ref={emailRef} />
-          <button className={styles.registerContainerFormSubmitButton} type="submit">Registrar</button>
+        <form className={styles.registerContainerForm} onSubmit={handleSubmit}>
+          <input
+            className={styles.registerContainerFormInput}
+            placeholder="Digite seu e-mail"
+            type="email"
+            onChange={(e) => {
+              setEmailReg(e.target.value)
+            }}
+          />
+          <button
+            className={styles.registerContainerFormSubmitButton}
+            type="submit"
+            onClick={registerUser}
+          >
+            Registrar
+          </button>
         </form>
         <div className={styles.registerContainerTerms}>
           <p className={styles.registerContainerTermsText}>
-            Ao se inscrever no Questty você concorda com os <Link to="/">Termos de Serviço</Link> e <Link to="/">Política de Privacidade do Questty</Link>.
+            Ao se inscrever no Questty você concorda com os{" "}
+            <Link to="/">Termos de Serviço</Link> e{" "}
+            <Link to="/">Política de Privacidade do Questty</Link>.
           </p>
         </div>
 
@@ -42,8 +59,16 @@ const SignUp = () => {
             Ou inscreva-se usando:
           </p>
           <div className={styles.registerContainerSocialRowIcons}>
-            <img className={styles.registerContainerSocialRowIconsImage} src={googleIcon} alt="" />
-            <img className={styles.registerContainerSocialRowIconsImage} src={facebookIcon} alt="" />
+            <img
+              className={styles.registerContainerSocialRowIconsImage}
+              src={googleIcon}
+              alt=""
+            />
+            <img
+              className={styles.registerContainerSocialRowIconsImage}
+              src={facebookIcon}
+              alt=""
+            />
           </div>
         </div>
       </div>
